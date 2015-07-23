829d569cbde7840a0a865b1e9307c95bad59aad0
Aaron Podolny <apodolny@gmail.com>
2015-07-23 09:36:27 -0400

    Change calculation of cache keys to take into account shipment address

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,7 +1,9 @@
 Spree::LineItem.class_eval do
   def tax_cloud_cache_key
     key = "Spree::LineItem #{id}: #{quantity}x<#{variant.cache_key}>@#{price}#{currency}"
-    if order.ship_address
+    if line_item.inventory_units.any?
+      key << "shipped_to<#{item.inventory_units.first.shipment.try(:cache_key)}>"
+    elsif order.ship_address
       key << "shipped_to<#{order.ship_address.try(:cache_key)}>"
     elsif order.billing_address
       key << "billed_to<#{order.bill_address.try(:cache_key)}>"
