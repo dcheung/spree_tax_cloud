aed91df5eb68d26f3850d19a85379424c899d9a4
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-08 11:48:10 -0700

    Update tax_cloud_calculator.rb

--- app/models/spree/calculator/tax_cloud_calculator.rb
+++ app/models/spree/calculator/tax_cloud_calculator.rb
@@ -13,29 +13,31 @@ module Spree
 
     # When it comes to computing shipments or line items: same same.
     def compute_shipment_or_line_item(item)
-      if rate.included_in_price
-        raise 'TaxCloud cannot calculate inclusive sales taxes.'
-      else
         round_to_two_places(tax_for_item(item))
-        # TODO take discounted_amount into account. This is a problem because TaxCloud API does not take discounts nor does it return percentage rates.
-      end
+#      if rate.included_in_price
+#        raise 'TaxCloud cannot calculate inclusive sales taxes.'
+#      else
+#        round_to_two_places(tax_for_item(item))
+#        # TODO take discounted_amount into account. This is a problem because TaxCloud API does not take discounts nor does it return percentage rates.
+#      end
     end
 
     alias_method :compute_shipment, :compute_shipment_or_line_item
     alias_method :compute_line_item, :compute_shipment_or_line_item
 
     def compute_shipping_rate(shipping_rate)
-      if rate.included_in_price
-        raise 'TaxCloud cannot calculate inclusive sales taxes.'
-      else
+#      if rate.included_in_price
+#        raise 'TaxCloud cannot calculate inclusive sales taxes.'
+#      else
         # Sales tax will be applied to the Shipment itself, rather than to the Shipping Rates.
         # Note that this method is called from ShippingRate.display_price, so if we returned
         # the shipping sales tax here, it would display as part of the display_price of the 
         # ShippingRate, which is not consistent with how US sales tax typically works -- i.e.,
         # it is an additional amount applied to a sale at the end, rather than being part of
         # the displayed cost of a good or service.
-        return 0
-      end
+#        return 0
+#      end
+       return 0
     end
 
     private
